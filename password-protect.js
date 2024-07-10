de3a9d722be5e1a4be45d5b6bc491036e5130aff
SafariBOT-E <safaribot@safari-solutions.com>
2024-07-10 14:41:45 -0400

    Update password-protect.js

--- password-protect.js
+++ password-protect.js
@@ -1,10 +1,10 @@
-const correctPasswordHash = 'b64d75539245c3ef0221ca1c7b07ec531cb3a393b330cb67b29a72df76a2021e'; // Hashed password
+const correctPasswordHash = '8be270c2e030806ea253e5a9700c792c910214c551a238d51e947fd30a6fc47a0424ceaf56d08f7c56da1aea8d0d079c9c989bdb2aca73e080ae54fa4071f238'; // Hashed password
 
 function hashPassword(password) {
-    // Simple hash function (e.g., SHA-256)
+    // Simple hash function (e.g., SHA-512)
     const encoder = new TextEncoder();
     const data = encoder.encode(password);
-    return crypto.subtle.digest('SHA-256', data).then(hash => {
+    return crypto.subtle.digest('SHA-512', data).then(hash => {
         let hashArray = Array.from(new Uint8Array(hash));
         let hashHex = hashArray.map(b => b.toString(16).padStart(2, '0')).join('');
         return hashHex;
